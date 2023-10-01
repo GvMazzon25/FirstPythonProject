a572288587e3da227097578c42ebdb3f0d2940a8
GvMazzon25 <gvmazzon25@gmail.com>
2023-10-01 22:39:01 +0200

    first result for do operation function

--- variable_equation.py
+++ variable_equation.py
@@ -41,7 +41,6 @@ def generate_variable(index, expression):
                     return variable
                 break
             elif expression[i].isdigit():
-                print(expression[i])
                 list_num.insert(0, expression[i])
 
         variable_number = "".join(list_num)
@@ -56,7 +55,6 @@ def create_variable_arr(indexes, expression):
         variable = generate_variable(i, expression)
         variable_arr.append(variable)
 
-    print(variable_arr)
     return variable_arr
 
 
@@ -85,15 +83,14 @@ def variable_transport(left_part, right_part, left_index, right_index):
             variable2 = Variable.Variable('*', variable.mult, variable.variable)
             right_arr.append(variable2)
             variable_left.append(variable2)
-    print(do_operation(variable_left))
-    print(do_operation(right_arr))
-    return variable_left, right_arr
+    left = do_operation(variable_left)
+    right = do_operation(right_arr)
+    return left, right
 
 
 def do_operation(array):
     first_var = array[0]
     second_var = array[1]
-    print(array[1].sign)
     if second_var is not []:
         sign1 = first_var.sign
         sign2 = second_var.sign
@@ -105,7 +102,6 @@ def do_operation(array):
             array[1].sign = '+'
 
     operation = ''
-
     for indice, variable in enumerate(array):
         if indice == 0:
             number = variable.mult
@@ -115,8 +111,8 @@ def do_operation(array):
             number = variable.mult
             operation = operation + sign + str(number)
 
-    print(operation)
-    return operation
+    result = reseq.create_result(operation)
+    return result
 
 
 def variable_operation(left_part, right_part):
@@ -138,7 +134,8 @@ def resolve_variable_equation(user_input):
         if element == '=':
             left_part = user_input[:counter]
             right_part = user_input[counter + 1:]
-            result = variable_operation(left_part, right_part)
+            left, right = variable_operation(left_part, right_part)
+            result = str(left) + '=' + str(right)
             break
         else:
             counter += 1
